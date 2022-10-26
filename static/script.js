85076adb95a5fcbc556787c485bd1e046c6ea8b0
Visal <visalrao19@gmail.com>
2022-10-26 20:44:02 +0530

    updated

---
+++ static/script.js
@@ -0,0 +1,41 @@
+/*
+
+inspiration: 
+https://dribbble.com/shots/2292415-Daily-UI-001-Day-001-Sign-Up
+
+*/
+// window.onload = function(){ 
+// document.getElementById("myButton").onclick = function () {
+//   console.log("loggedIN");
+  
+//   var username = document.getElementById("username").value;
+//   var password = document.getElementById("password").value;
+//   let person = window.localStorage.getItem('users');
+//   if(person == null) {
+//     window.localStorage.setItem('users',JSON.stringify([{username,password}]));
+//   } else {
+//     person = JSON.parse(person)
+//     let updatedPerson = [{username,password},...person]
+//     window.localStorage.setItem('users',JSON.stringify(updatedPerson));
+//   }
+//   sessionStorage.setItem("currentloggedin",username);
+//   location.href = "/home/";
+// };
+// };
+
+
+
+// document.getElementById("searchID").onclick = function () {
+//   console.log("datatta");
+//   const searchinput = document.getElementById("searchInput");
+//   console.log(searchinput);
+// }
+
+
+// let form = document.querySelecter('form');
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   console.log("abcd");
+//   return false;
+// });
